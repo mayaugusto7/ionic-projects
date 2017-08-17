3386fcf9ccc75c8f984dfafd6518217e3131ed1d
Maycon Ribeiro <maycon@ilion.com.br>
2017-08-17 18:23:44 -0300

    first commit

---
+++ hello/platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,28 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "es6-promise-plugin.Promise",
+        "file": "plugins/es6-promise-plugin/www/promise.js",
+        "pluginId": "es6-promise-plugin",
+        "runs": true
+    },
+    {
+        "id": "phonegap-plugin-media-stream.MediaDevices",
+        "file": "plugins/phonegap-plugin-media-stream/www/android/MediaDevices.js",
+        "pluginId": "phonegap-plugin-media-stream",
+        "clobbers": [
+            "navigator.mediaDevices"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.2",
+    "es6-promise-plugin": "4.1.0",
+    "cordova-plugin-compat": "1.1.0",
+    "phonegap-plugin-media-stream": "1.1.0",
+    "phonegap-plugin-image-capture": "1.1.0"
+};
+// BOTTOM OF METADATA
+});
